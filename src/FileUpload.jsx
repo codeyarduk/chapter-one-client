ae42568aab10de1d66dbd3cbf9d83daf2b70e7d9
davidwrossiter <davewrossiter@gmail.com>
2024-03-28 13:07:28 +0000

    Refactor file upload function to use fetch API with streaming

--- src/FileUpload.jsx
+++ src/FileUpload.jsx
@@ -9,24 +9,25 @@ const FileUpload = () => {
     setFile(event.target.files[0]);
   };
 
-  const onFileUpload = async () => {
+  const onFileUpload = () => {
     const formData = new FormData();
     formData.append("file", file);
-    try {
-      const response = await axios.post(
-        "http://localhost:3000/upload",
-        formData
-      );
-      console.log(response.data);
-      setReview(response.data);
-    } catch (error) {
-      console.error(error);
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-      }
-    }
+
+    fetch("http://localhost:3000/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder("utf-8");
+
+        reader.read().then(function process({ done, value }) {
+          if (done) return;
+          setReview((prevReview) => prevReview + decoder.decode(value));
+          return reader.read().then(process);
+        });
+      })
+      .catch((err) => console.error(err));
   };
 
   return (
