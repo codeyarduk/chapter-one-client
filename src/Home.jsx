9c26f1cadb8b61298e691ccc90b844c0152c748f
davidwrossiter <davewrossiter@gmail.com>
2024-04-01 16:01:38 +0100

    Update Home.jsx with AI-related content

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,19 +3,22 @@ import React from "react";
 function Home() {
   return (
     <div className="z-[100]">
-      <div className="h-96 bg-black flex justify-center items-center pt-20">
+      <div className="h-96 bg-black text-white flex-col flex justify-center items-center pt-20">
         <h1 className="text-white text-4xl font-bold font-archivoBlack">
           Chapter One AI
         </h1>
+        <p className="mt-5">
+            Start the next chapter of your career with the power of AI
+        </p>
       </div>
       <div>
-        <h2 className="text-2xl font-semibold text-center h-96 flex justify-center items-center">
-          Start the next chapter of your career with the power of AI
+        <h2 className="text-2xl italic font-semibold text-center h-96 flex justify-center items-center">
+          /Video of me explaining how it works/
         </h2>
       </div>
       <div className="h-96 bg-black flex justify-center items-center">
         <h1 className="text-white text-4xl font-bold font-archivoBlack">
-          Get your CV reviewed by AI
+            Tailor your resume for each different job that you apply for
         </h1>
       </div>
       <div>
