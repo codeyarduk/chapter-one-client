55016db1ff5c9ca44a806b19aafe87aa9f57b808
davidwrossiter <davewrossiter@gmail.com>
2024-04-03 11:45:42 +0100

    Add Google Sign-In and Profile Page

---
+++ src/LoginOauth.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { jwtDecode as jwt_decode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+import React from "react";
+
+function LoginOauth() {
+  const [user, setUser] = useState({});
+  const [outcome, setOutCome] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleCallback(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    sendEmail(response.credential);
+    setUser(userObject);
+    navigate("/profile", {
+      state: {
+        email: userObject.email,
+        firstName: userObject.given_name,
+        lastName: userObject.family_name,
+        uses: 0,
+      },
+    });
+  }
+
+  const sendEmail = (userObject) => {
+    fetch("http://localhost:3000/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: userObject,
+      }),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setOutCome(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id:
+        "886756526696-8pc6lu70409d3uu0jvfkojk02kjoak7t.apps.googleusercontent.com",
+      callback: handleCallback,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
+  return (
+    <div className="pt-20">
+      <div id="signInDiv"></div>
+      {user && (
+        <div>
+          {/* <img src={user.picture}></img> */}
+          <h3>{outcome}</h3>
+
+          {/* <h4 >{user.email}</h4> */}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default LoginOauth;
---
+++ src/Profile.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+// { email, name, uses }
+function Profile() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const email = location.state?.email;
+  const firstName = location.state?.firstName;
+  const lastName = location.state?.lastName;
+  const uses = location.state?.uses;
+
+  useEffect(() => {
+    if (!email) {
+      navigate("/login");
+    }
+  }, [email, firstName, lastName, uses, navigate]);
+
+  if (!email) {
+    // navigate("/login");
+    // return null;
+    console.log("No email");
+    return (
+      <div className="pt-20">
+        <h2>Profile</h2>
+        <p>Not logged in</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="pt-20">
+      <h2>Profile</h2>
+      <p>Email: {email}</p>
+      <p>Name: {firstName}</p>
+      <p>Last name: {lastName}</p>
+      <p>Uses: {uses}</p>
+    </div>
+  );
+}
+
+export default Profile;
